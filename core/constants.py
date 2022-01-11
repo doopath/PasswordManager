9a690c1a3d00c10ba64b5e1271f9f274f382a780
Michael Nikishov <doopath@gmail.com>
2022-01-11 11:59:06 +0300

    help list fix

--- core/constants.py
+++ core/constants.py
@@ -15,7 +15,7 @@ HELP_LIST = "\n".join([
     "# --set-value | -sv <name> <value> <password?>       | set a value of a property                 #",
     "# --add-property | -ap <name> <value> <password?>    | add a <name> <value> pair to the store    #",
     "# --remove-property | -rp <name> <value> <password?> | add a <name> <value> pair to the store    #",
-    "# --make-backup | -mb <password?>                    | make a backup of the current store        #",
+    "# --make-backup | -mb                                | make a backup of the current store        #",
     "# --show-store | -ss <path?> <password?>             | show a store on <path> or the current one #",
     "##################################################################################################"
 ])
